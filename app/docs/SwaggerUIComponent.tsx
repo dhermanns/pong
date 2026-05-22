0369d306f888ac3b859a2504cbaf66bf4924d922
dhermanns <h3rmanns@gmail.com>
2026-05-22 15:54:22 +0200

    Fix package.json

--- app/docs/SwaggerUIComponent.tsx
+++ app/docs/SwaggerUIComponent.tsx
@@ -3,6 +3,6 @@
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
-export default function SwaggerUIComponent({ spec }: { spec: any }) {
+export default function SwaggerUIComponent({ spec }: { spec: object | string }) {
   return <SwaggerUI spec={spec} />;
 }
